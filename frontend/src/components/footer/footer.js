522179fd6ce67b5a6768850ae3441bbbafe36c91
Canariiii <josembravomendez@gmail.com>
2023-11-28 16:32:59 +0000

    some fixes

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './footer.css';
+
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className="footer-container">
+				
+			</div>
+		</footer>
+	)
+}
+
+export default Footer;
--- frontend/src/pages/profile/profile.js
+++ frontend/src/pages/profile/profile.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import UserProfile from '../../components/userProfile/userProfile';
 import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
 
 function Profile() {
   return (
     <>
       <Header />
       <UserProfile />
+      <Footer />
     </>
   );
 }
