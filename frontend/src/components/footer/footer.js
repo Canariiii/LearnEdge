a6d472ac74696438a5957d2175ec4fac2506afab
Canariiii <josembravomendez@gmail.com>
2023-12-04 16:50:52 +0000

    footer created

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+    return (
+        <footer className='footer-container'>
+            <div className='footer-line'></div>
+            <p>© 2023 Learn Edge</p>
+            <img src='assets/img/logo.png' alt='footer-logo'></img>
+        </footer>
+    );
+}
+
+export default Footer;
