bc9207351972333062f8e12f7248a8be253c3dd1
Canariiii <josembravomendez@gmail.com>
2023-12-18 20:12:52 +0000

    test fixed

--- backend/test/userController.test.js
+++ backend/test/userController.test.js
@@ -16,17 +16,52 @@ afterAll(async () => {
 });
 
 describe('UserController', () => {
-  it('Error', async () => {
+  it('User created', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send({
+        username: 'Jose',
+        email: 'test@prueba.es',
+        password: '8guhgy78678gy',
+        phone: '123121212',
+        role: 'student',
+        filename: 'facets-1.png'
+      });
+      expect(response.statusCode).toBe(201);
+      expect(response.body.success).toBe(true);
+      expect(response.body.data.user).toHaveProperty('username');
+      expect(response.body.data.user.username).toBe('Jose');
+  })
+});
+
+describe('UserController', () => {
+  it('Error 500', async () => {
     const response = await request(app)
       .post('/users')
       .send({
         username: 'Jose',
         email: 'test@prueba.es',
         password: '1234',
-        phone: '1234',
-        role: 'none'
+        phone: '123121212',
+        role: 'instructor',
+        filename: 'facets-1.png'
       });
     expect(response.statusCode).toBe(500);
     expect(response.body.success).toBe(false);
   })
 });
+
+describe('UserController', () => {
+    
+
+  // Test para obtener todos los runners
+  it('All users', async () => {
+    const response = await request(app)
+      .get('/users');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.data).toBeInstanceOf(Array);
+  });
+});
+
